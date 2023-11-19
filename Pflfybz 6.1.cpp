09b282998924d50b4a46f8af2ea62f9940a13862
Siskis <146063514+Siskis@users.noreply.github.com>
2023-11-19 09:35:49 +0300

    Update Pflfybz 6.1.cpp

--- Pflfybz 6.1.cpp
+++ Pflfybz 6.1.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 int main()
 {
-    int tablicha[9][9];
+    int tablicha[9][9]; \\ двумерный массив
     setlocale(0, "");
 
     for (int i = 0; i < 9; i++)
     {
         for (int k = 0; k < 9; k++)
         {
-            tablicha[i][k] = (i + 1) * (k + 1);
+            tablicha[i][k] = (i + 1) * (k + 1); \\каждый элемент массива tablicha будет умножаться на номер соответствующей итерации в каждом из циклов.
+
         }
     }
 
