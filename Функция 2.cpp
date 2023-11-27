21457f4be146e1ae93925f98bd776c87fb00f595
Siskis <146063514+Siskis@users.noreply.github.com>
2023-11-27 23:15:26 +0300

    Add files via upload

---
+++ Функция 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "Введите число n: ";
+    cin >> n;
+
+    while (n >= 10) {
+        int t = 0;
+        while (n != 0)
+        {
+            t  =t + n % 10;
+            n =n / 10;
+        }
+        n = t;
+    }
+
+    cout << "Однозначное число: " << n << endl;
+
+    return 0;
+}
